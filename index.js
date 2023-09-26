19a0b3815a0945319d4b1c79afd97ee756892bbc
Sourav6803 <106535807+Sourav6803@users.noreply.github.com>
2023-09-26 15:50:33 +0530

    Update index.js
    ghgdg

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ const cors = require("cors");
 const path = require("path");
 
 app.use(bodyParser.json());
-app.use(cors({
-    origin : "https://main.d2mckntz38xq4s.amplifyapp.com",
-    credentials: true
-})
+app.use(cors())
 
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
